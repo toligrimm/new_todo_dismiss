940fc7ed349fd87edf9bbeb61ea7d87be4d4d50a
toligrimm <72781269+toligrimm@users.noreply.github.com>
2022-04-12 21:50:33 +0600

    u

--- lib/classes/todo_list.dart
+++ lib/classes/todo_list.dart
@@ -3,220 +3,163 @@ import 'package:dismiss/classes/todo.dart';
 import 'package:flutter/material.dart';
 import '../menu/my_chat.dart';
 import '../menu/my_settings.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 
 class ToDoList1 extends StatefulWidget {
-
   static const String tag = 'todo-list-use';
 
   const ToDoList1({Key? key}) : super(key: key);
 
   @override
   _ToDoList1State createState() => _ToDoList1State();
-
 }
 
 class _ToDoList1State extends State<ToDoList1> {
-
   final List<Todo1> _todoList = [];
   Map<int, Todo1> _todoMap = {};
   bool showTextFormField = false;
-  int _selectedIndex = 0;
-  int _saver = 0;
-  // static const TextStyle optionStyle =
-  // TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  // static const List<Widget> _widgetOptions = <Widget>[
-  //
-  //   Text(
-  //     'Index 0: Home',
-  //     style: optionStyle,
-  //   ),
-  //   Text(
-  //     'Index 1: Business',
-  //     style: optionStyle,
-  //   ),
-  //   Text(
-  //     'Index 2: School',
-  //     style: optionStyle,
-  //   ),
-  // ];
 
-  void _onItemTapped(int index) {
+  void _addNewTodo() {
     setState(() {
-      _selectedIndex = index;
+      _todoList.add(Todo1('', 1));
     });
   }
-    void _addNewTodo() async {
-      final prefs = await SharedPreferences.getInstance();
-      setState(() {
-        _todoList.add(Todo1('', 1, ));
-        _saver = (prefs.getInt('saver') ?? 0);
-      });
-    }
+
+
   @override
-  void initState() {
-    super.initState();
-    _addNewTodo();
-  }
-
-
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              const SizedBox(
-                height: 90,
-                child: DrawerHeader(
-
-                  child: Text(
-                    'Меню',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.pink,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const SizedBox(
+              height: 90,
+              child: DrawerHeader(
+                child: Text(
+                  'Меню',
+                  style: TextStyle(
+                    color: Colors.white,
                   ),
                 ),
+                decoration: BoxDecoration(
+                  color: Colors.pink,
+                ),
               ),
+            ),
 
-              ListTile(
-                trailing: const Icon(
-                  Icons.person,
-                  color: Colors.white,
-                ),
-                title: const Text('Мой профиль'),
-                tileColor: Colors.lightBlueAccent,
-                textColor: Colors.white,
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const MyProfilePage()),
-                  );
-                },
+            ListTile(
+              trailing: const Icon(
+                Icons.person,
+                color: Colors.white,
               ),
+              title: const Text('Мой профиль'),
+              tileColor: Colors.lightBlueAccent,
+              textColor: Colors.white,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const MyProfilePage()),
+                );
+              },
+            ),
 
-              ListTile(
-                title: const Text('Чат'),
-                tileColor: Colors.orange,
-                textColor: Colors.white,
-                trailing: const Icon(
-                  Icons.chat,
-                  color: Colors.white,
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const ChatPage()),
-                  );
-                },
+            ListTile(
+              title: const Text('Чат'),
+              tileColor: Colors.orange,
+              textColor: Colors.white,
+              trailing: const Icon(
+                Icons.chat,
+                color: Colors.white,
               ),
-              ListTile(
-                title: const Text('Настройки'),
-                tileColor: Colors.green,
-                textColor: Colors.white,
-                trailing: const Icon(
-                  Icons.settings,
-                  color: Colors.white,
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SettingsPage()),
-                  );
-                },
-              )
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ChatPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Настройки'),
+              tileColor: Colors.green,
+              textColor: Colors.white,
+              trailing: const Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SettingsPage()),
+                );
+              },
+            )
             ],
-          ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            _addNewTodo();
-          },
-          child: const Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        appBar: AppBar(
-          title: const Text('ToDo List'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addNewTodo();
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
         ),
-        body: Container(
-          child: Builder(
-            builder: (context) {
-              // _widgetOptions.elementAt(_selectedIndex);
-              print("List : ${_todoList.toString()}");
-              _todoMap = _todoList.asMap();
-              print("MAP : '${_todoMap.toString()}"); //не удалять
-              return Scrollbar(
-                isAlwaysShown: true,
-                child: ListView.builder(
-                  padding: const EdgeInsets.only(top: 10),
-                  itemCount: _todoMap.length,
-
-                  itemBuilder: (context, position) {
-                    return Padding(
-                      padding: const EdgeInsets.fromLTRB(10, 3, 3, 5),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Expanded(
-                            child: TextFormField(
-                              decoration: const InputDecoration(
-                                hintText: 'Введите значение',
-                                hintStyle: TextStyle(
-                                  fontSize: 16.0,
-                                  color: Colors.black26,
-                                ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      appBar: AppBar(
+        title: const Text('ToDo List'),
+      ),
+      body: Container(
+        child: Builder(
+          builder: (context) {
+            print("List : ${_todoList.toString()}");
+            _todoMap = _todoList.asMap();
+            print("MAP : ${_todoMap.toString()}"); //не удалять
+            return Scrollbar(
+              isAlwaysShown: true,
+              child: ListView.builder(
+                padding: const EdgeInsets.only(top: 10),
+                
+                itemCount: _todoMap.length,
+                itemBuilder: (context, position) {
+                  return Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 3, 3, 5),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Expanded(
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: 'Введите значение',
+                              hintStyle: TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.black26,
                               ),
                             ),
                           ),
-                          IconButton(
-                            icon: const Icon(
-                              Icons.delete_outline,
-                              color: Colors.pink,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _todoList.removeAt(position);
-                              });
-                            },
+                        ),
+                        IconButton(
+                          icon: const Icon(
+                            Icons.delete_outline,
+                            color: Colors.pink,
                           ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              );
-            },
-          ),
+                          onPressed: () {
+                            setState(() {
+                              _todoList.removeAt(position);
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            );
+          },
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'Business',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              label: 'School',
-            ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.amber[800],
-          onTap: _onItemTapped,
-        ),
-      );
-    }
+      ),
+    );
   }
+}
--- lib/menu/my_chat.dart
+++ lib/menu/my_chat.dart
@@ -1,21 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ChatPage extends StatelessWidget {
+
+class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
+
+  @override
+  _ChatPageState createState() => _ChatPageState();
+
+}
+
+class _ChatPageState extends State<ChatPage> {
+  int _saver = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSaved();
+  }
+
+  void _loadSaved() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _saver = (prefs.getInt('counter') ?? 0);
+    });
+  }
+
+  void _incrementSaved() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _saver = (prefs.getInt('counter') ?? 0) + 1;
+      prefs.setInt('counter', _saver);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Чат'),
+        title: Text('count'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Иди назад!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_saver',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementSaved,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
